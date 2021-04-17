7bf6813821f3edbd0b57c4c8a39e4699966ec083
James Talbot <36442996+JTBuddy@users.noreply.github.com>
2021-04-17 15:05:10 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <html>
   <head>
-    <title> Index </title>
+         <title>Infographics Final Project</title>
+
+        <!-- Bootstrap CDN and CSS -->
+        <link rel="stylesheet" href="Styles.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <!-- Fonts -->
+        <!-- Montserrat -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
   </head>
   <body>
     <h1> First Website </h1>

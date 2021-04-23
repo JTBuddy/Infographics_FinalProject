a381203144cd89662c2c5fa11e35d7cc15a9350f
James Talbot <36442996+JTBuddy@users.noreply.github.com>
2021-04-23 15:25:37 -0700

    Update Tabs.php
    added br after

--- Tabs.php
+++ Tabs.php
@@ -13,7 +13,7 @@
 <div class="container">
   <h2>iPhone vs Android</h2>
   <p style="padding: 40px;">
-  Below show the interest of both iPhone and Adroid on Google. The values are displayed as such: 
+  Below show the interest of both iPhone and Adroid on Google. The values are displayed as such: <br>
     <i>
     Numbers represent search interest relative to the highest point on the chart for the given region and time. A value of 100 is the peak popularity for the term. A value of 50 means that the term is half as popular. A score of 0 means there was not enough data for this term.
     </i>

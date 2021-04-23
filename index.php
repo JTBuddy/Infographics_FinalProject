0ade6082a842f76aec7ae07ddab41698c39d99c2
James Talbot <36442996+JTBuddy@users.noreply.github.com>
2021-04-23 15:21:16 -0700

    Update index.php

--- index.php
+++ index.php
@@ -43,12 +43,7 @@
                   <div>
                     <p style="padding: 40px;">With the explosive popularity with social media apps like Facebook, Instagram and Twitter I believe that this one of the main catalysts of the rapid growth of mobile phone usage.</p>
                   </div>
-                  
-                  <!-- Android vs iPhone from Google Trends Comparison -->
-                  <script type="text/javascript" src="https://ssl.gstatic.com/trends_nrtr/2431_RC04/embed_loader.js"></script> <script type="text/javascript"> trends.embed.renderExploreWidget("TIMESERIES", {"comparisonItem":[{"keyword":"/m/027lnzs","geo":"","time":"all"},{"keyword":"Android","geo":"","time":"all"}],"category":0,"property":""}, {"exploreQuery":"date=all&q=%2Fm%2F027lnzs,Android","guestPath":"https://trends.google.com:443/trends/embed/"}); </script>
-          
-                  <br>
-                  
+               
                   <?php include 'Tabs.php'?>
                   
                 </div>

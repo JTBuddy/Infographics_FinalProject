5e098d2c8f3005fce20f84fd391ee7b9c446502d
James Talbot <36442996+JTBuddy@users.noreply.github.com>
2021-04-23 13:34:21 -0700

    Update index.php
    Added iPhone and Android charts from Google trends

--- index.php
+++ index.php
@@ -44,6 +44,12 @@
                     <p>With the explosive popularity with social media apps like Facebook, Instagram and Twitter I believe that this one of the main catalysts of the rapid growth of mobile phone usage.</p>
                   </div>
                   
+                  <!-- Android vs iPhone from Google Trends -->
+                  <!-- iPhone -->
+                  <script type="text/javascript" src="https://ssl.gstatic.com/trends_nrtr/2431_RC04/embed_loader.js"></script> <script type="text/javascript"> trends.embed.renderExploreWidget("TIMESERIES", {"comparisonItem":[{"keyword":"/m/027lnzs","geo":"","time":"2004-01-01 2021-04-23"}],"category":0,"property":""}, {"exploreQuery":"date=all&q=%2Fm%2F027lnzs","guestPath":"https://trends.google.com:443/trends/embed/"}); </script>
+                  
+                  <!-- Android -->
+                  <script type="text/javascript" src="https://ssl.gstatic.com/trends_nrtr/2431_RC04/embed_loader.js"></script> <script type="text/javascript"> trends.embed.renderExploreWidget("TIMESERIES", {"comparisonItem":[{"keyword":"Android","geo":"US","time":"2004-01-01 2021-04-23"}],"category":0,"property":""}, {"exploreQuery":"date=all&geo=US&q=Android","guestPath":"https://trends.google.com:443/trends/embed/"}); </script>
                 </div>
 
                 <div class="row" id="section3">

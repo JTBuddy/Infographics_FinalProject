4bde85da8802d3b7291ea45b1e26fda8962fe64a
James Talbot <36442996+JTBuddy@users.noreply.github.com>
2021-04-23 15:38:30 -0700

    Update Tabs.php

--- Tabs.php
+++ Tabs.php
@@ -12,12 +12,7 @@
 
 <div class="container">
   <h2>iPhone vs Android</h2>
-  <p style="padding: 40px; font-size: 18px; margin-top: -25px;">
-  Below show the interest of both iPhone and Adroid on Google. The values are displayed as such: <br>
-    <i>
-    Numbers represent search interest relative to the highest point on the chart for the given region and time. A value of 100 is the peak popularity for the term. A value of 50 means that the term is half as popular. A score of 0 means there was not enough data for this term.
-    </i>
-  </p>
+  
   <ul class="nav nav-pills">
     <li style="font-size: 25px;" class="active"><a data-toggle="pill" href="#home">Index</a></li>
     <li style="font-size: 25px;"><a data-toggle="pill" href="#menu1">Comparison</a></li>
@@ -25,11 +20,18 @@
   
   <div class="tab-content">
     <div id="home" class="tab-pane fade in active">
-      <h3>HOME</h3>
+      <h3>Pie Chart</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </div>
     
     <div id="menu1" class="tab-pane fade">
+    <p style="padding: 40px; font-size: 18px; margin-top: -25px;">
+    Below show the interest of both iPhone and Adroid on Google. The values are displayed as such: <br>
+      <i>
+      Numbers represent search interest relative to the highest point on the chart for the given region and time. A value of 100 is the peak popularity for the term. A value of 50 means that the term is half as popular. A score of 0 means there was not enough data for this term.
+      </i>
+    </p>
+      
  <!-- Android vs iPhone from Google Trends Comparison -->
                   <script type="text/javascript" src="https://ssl.gstatic.com/trends_nrtr/2431_RC04/embed_loader.js"></script> <script type="text/javascript"> trends.embed.renderExploreWidget("TIMESERIES", {"comparisonItem":[{"keyword":"/m/027lnzs","geo":"","time":"all"},{"keyword":"Android","geo":"","time":"all"}],"category":0,"property":""}, {"exploreQuery":"date=all&q=%2Fm%2F027lnzs,Android","guestPath":"https://trends.google.com:443/trends/embed/"}); </script>
     </div>
